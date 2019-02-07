18e6e900a0688cf27eafa39abc95e3cb8558c86c
James Morgan <jamiem842@gmail.com>
2019-02-07 21:35:05 +0000

    Fake news keywords added in database + endpoint created

--- articleExtractor.py
+++ articleExtractor.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from json import dumps
 from flask import jsonify
 from newspaper import Article
+import nltk
 import requests
 from flask_cors import CORS, cross_origin
 
@@ -35,9 +36,39 @@ class blacklistedSites(Resource):
         query = conn.execute("select * from blacklist")
         return {'blacklistedSites': [i[1] for i in query.cursor.fetchall()]}
 
+class keywords(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query = conn.execute("select * from keywords")
+        return {'keywords': [i[1] for i in query.cursor.fetchall()]}
+
 api.add_resource(whitelistedSites, '/whitelistedSites')
 api.add_resource(satiricalSites, '/satiricalSites')
 api.add_resource(blacklistedSites, '/blacklistedSites')
+
+api.add_resource(keywords, '/keywords')
+
+@app.route('/keywords', methods=['POST'])
+@cross_origin(supports_credentials=True)
+def test5():
+
+    f = []
+
+    text = request.form.get('text')
+
+    conn = db_connect.connect()
+
+    query = conn.execute("select * from keywords")
+
+    for i in query.cursor.fetchall():
+
+        if i[1] in text:
+
+            f.append(i)
+
+    result = {'keywords': [i[1] for i in f]}
+
+    return jsonify(result)
 
 @app.route('/whitelistedSites', methods=['POST'])
 @cross_origin(supports_credentials=True)
@@ -82,6 +113,8 @@ def test4():
 @cross_origin(supports_credentials=True)
 def test():
 
+    nltk.download('punkt')
+
     url = request.form.get('url')
 
     article = Article(url)
@@ -89,6 +122,8 @@ def test():
     article.download()
 
     article.parse()
+
+    article.nlp()
 
     return jsonify(title=article.title,
                    publishDate=article.publish_date,
